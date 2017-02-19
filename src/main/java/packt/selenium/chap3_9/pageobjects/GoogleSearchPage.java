e9fd555c92cb3c6dfe8946825c14b0fbe07e069a
ss22740 <saurabh@Saurabhs-MacBook-Pro-2.local>
2017-02-19 13:42:11 -0500

    adding all files

---
+++ src/main/java/packt/selenium/chap3_9/pageobjects/GoogleSearchPage.java
@@ -0,0 +1,24 @@
+package packt.selenium.chap3_9.pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by Ripon on 11/19/2015.
+ */
+public class GoogleSearchPage {
+    private WebDriver driver;
+    public GoogleSearchPage(WebDriver driver){
+        this.driver = driver;
+        System.out.println(driver.getTitle());
+        if (!driver.getTitle().equals("Mastering Selenium Testing Tools - Google Search")){
+            throw new WrongPageException("Incorrect page for Google Search page");
+        }
+    }
+    public String getNumberOfResults(){
+        String numberOfResults = driver.findElement(By.id("resultStats")).getText();
+        System.out.println(numberOfResults);
+        return  numberOfResults;
+    }
+    // hey dad, fyi, you can use the "sout" to get the printing function, if you don't know
+}
